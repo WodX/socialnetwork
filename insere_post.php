9d2d2737d3ea9e1047d21c5318c865114cc5e0f5
André Nunes <31810024+WodX@users.noreply.github.com>
2021-01-28 13:54:09 +0000

    Update insere_post.php

--- insere_post.php
+++ insere_post.php
@@ -9,10 +9,12 @@ if(!isset($_SESSION['loggedIN'])){
 
 $UserId = $_SESSION['User_id'];
 
-$servername = "localhost";
-$username = "andre";
-$password = "andre.2019";
-$dbname = "andrenun_work";
+// include "includes/connection.php"; -- Use this --
+
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
